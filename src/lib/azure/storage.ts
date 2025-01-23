4f4d1cc2746fa15ade9c94ce57d2c0f4d83f9326
jgomes79 <jgomes79@hotmail.es>
2025-01-23 17:12:36 +0100

    feat(main): added azure storage upload function

---
+++ src/lib/azure/storage.ts
@@ -0,0 +1,30 @@
+import * as path from 'path';
+
+import { BlobServiceClient } from '@azure/storage-blob';
+import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class AzureStorageService {
+
+  async uploadImageToAzure(file: Express.Multer.File): Promise<string> {
+    const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+    const containerName = process.env.AZURE_STORAGE_NAME;
+
+    if (!connectionString || !containerName) {
+      throw new Error('Azure Storage connection string or container name is not configured.');
+    }
+
+    const extension = path.extname(file.originalname);
+    const randomString = uuidv4();
+    const name = `${randomString}${extension}`;
+
+    const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const blockBlobClient = containerClient.getBlockBlobClient(name);
+
+    await blockBlobClient.upload(file.buffer, file.buffer.length);
+
+    return blockBlobClient.url;
+  }
+}
